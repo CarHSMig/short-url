e522c96ee79e85fc5f958b59c0b58016a0547e99
CarHSMig <carlos.henrique@nullbug.dev>
2025-03-17 08:04:31 -0300

    Coerreções do Model de urls

--- migrations/20250311020925-create-urls.js
+++ migrations/20250311020925-create-urls.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Urls", {
+    await queryInterface.createTable("urls", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
--- models/url.js
+++ models/url.js
@@ -1,27 +1,32 @@
 'use strict';
+
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Url extends Model {
     static associate(models) {
     }
   }
   Url.init({
-    short_url: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-    {
-      original_url: DataTypes.STRING,
+    short_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    original_url: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
+  },
     {
       sequelize,
       modelName: "Url",
       tableName: "urls",
-      timestamps: false,
-    });
+      timestamps: true,
+    }
+  );
 
   return Url;
 };
