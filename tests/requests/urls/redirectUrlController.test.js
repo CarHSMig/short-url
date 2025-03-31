5dd49c50eb2a08392b90dbd5b055aebc13ab9942
CarHSMig <carlos.henrique@nullbug.dev>
2025-03-31 07:50:35 -0300

    criação dos testes em jest para o redirecionamento de url

---
+++ tests/requests/urls/redirectUrlController.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("../../../server");
+const { Url } = require("../../../models");
+
+jest.mock("../../../models");
+
+describe("#redirectUrlController", () => {
+  it("redirect to original URL ", async () => {
+    Url.findOne.mockResolvedValue({
+      original_url: "https://www.google.com",
+    });
+
+    const response = await request(app)
+      .post("/shorten")
+      .send({ original_url: "https://www.google.com" });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("short_url");
+  });
+
+  it("return 400 if invalid url", async () => {
+    const response = await request(app).post("/shorten").send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("originalUrl is required");
+  });
+});
